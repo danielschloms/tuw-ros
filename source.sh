ee51fe934db319ab318e9f5d128be21114b0565b
Daniel Schloms <daniel.schloms@gmail.com>
2024-03-14 16:33:47 +0100

    Source new ws folder

--- source.sh
+++ source.sh
@@ -3,3 +3,4 @@ echo "** ROS2 $ROS_DISTRO initialized with $RMW_IMPLEMENTATION **"
 source_ws ${PROJECT_DIR}/ws00/install/setup.bash
 source_ws ${PROJECT_DIR}/ws01/install/setup.bash
 source_ws ${PROJECT_DIR}/ws02/install/setup.bash
+source_ws ${PROJECT_DIR}/ws_kronecker/install/setup.bash
